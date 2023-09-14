e94625aa98d289921cef20cab134e88a15bf4b74
User01 <user01@gmail.com>
2023-09-14 12:28:52 +0700

    first deploy

---
+++ offline.js
@@ -0,0 +1,31 @@
+﻿{
+	"version": 1694669226,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/sprite-sheet0.png",
+		"images/title-sheet0.png",
+		"images/btn-sheet0.png",
+		"images/gun-sheet0.png",
+		"images/bullet-sheet0.png",
+		"images/battery-sheet0.png",
+		"images/battery-sheet1.png",
+		"images/aim-sheet0.png",
+		"images/alien-sheet0.png",
+		"images/target-sheet0.png",
+		"images/target-sheet1.png",
+		"images/how-sheet0.png",
+		"media/click.m4a",
+		"media/click.ogg",
+		"media/mouse click - sound effect (hd)-[audiotrimmer.com].m4a",
+		"media/mouse click - sound effect (hd)-[audiotrimmer.com].ogg",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
